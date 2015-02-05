e7bfeef38af3d3d5714a86beb4e2109788d1daed
Diogo Silva <diogo@diogok.net>
2015-02-05 01:46:53 -0200

    version up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dwc-analysis "0.0.6"
+(defproject dwc-analysis "0.0.10"
   :description "Analysis on darwincore occurrences: EOO, AOO and Populations."
   :url "http://github.com/CNCFlora/dwc-analysis"
   :license {:name "MIT"}
