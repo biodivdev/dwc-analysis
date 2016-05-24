8c1f28b5e1bf879d36eee9e93acd1a67b21159f8
Diogo Silva <diogo@diogok.net>
2016-05-24 20:20:32 -0300

    dependencies update

--- bench.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-time java -server -XX:+UseConcMarkSweepGC -XX:+UseCompressedOops -XX:+DoEscapeAnalysis -Xmx4G -cp "'$(lein classpath)'" clojure.main bench.clj
-
--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject dwc-analysis "0.0.27"
+(defproject dwc-analysis "0.0.28"
   :description "Analysis on darwincore occurrences: EOO, AOO and Populations."
   :url "http://github.com/CNCFlora/dwc-analysis"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [prismatic/plumbing "0.3.7"]
-                 [diogok/cljts "0.4.3"]]
-  :repositories [["clojars" {:sign-releases false}]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [prismatic/plumbing "0.5.3"]
+                 [org.clojars.diogok/cljts "0.4.7"]]
+  :repositories [["osgeo" "http://download.osgeo.org/webdav/geotools/"]
+                 ["clojars" {:sign-releases false}]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}})
